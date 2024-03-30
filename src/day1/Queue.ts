185a083b8e7759116557d5cc753dc039cba19e3b
Angel Ivanov <angel.iwanov@gmail.com>
2024-03-30 14:14:23 +0200

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,53 @@
+type Node<T> = {
+    value: T;
+    next: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        if (!this.head.next) {
+            this.head = this.tail = undefined;
+        } else {
+            this.head = this.head.next;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined;
+        }
+        return this.head.value;
+    }
 }
