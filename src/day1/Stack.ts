8276f9d3bd5948348e4739af49550e053c57a488
Angel Ivanov <angel.iwanov@gmail.com>
2024-04-01 06:00:33 +0300

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    value: T;
+    prev: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        if (!this.head.prev) {
+            this.head = undefined;
+        } else {
+            this.head = this.head.prev;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined;
+        }
+        return this.head.value;
+    }
 }
