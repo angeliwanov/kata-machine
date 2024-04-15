475fd82937b39367dec8f20691ac6c31580d38cf
Angel Ivanov <angel.iwanov@gmail.com>
2024-04-15 06:40:55 +0300

    BTBFS CompareBinaryTrees

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,18 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import { formatDiagnosticsWithColorAndContext } from "typescript";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
+
+    while (q.length) {
+        const curr = q.shift();
+
+        if (curr) {
+            if (curr.value === needle) return true;
+
+            q.push(curr.left);
+            q.push(curr.right);
+        }
+    }
+
+    return false;
 }
